5becd85d3f1b28fa21b8e7b7cee99689569340b3
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-04-06 20:12:10 -0300

    Criado o header da listagem de contatos
    Foi criado o header da listagem e definida as cores primárias da aplicação

--- fe/src/assets/styles/themes/default.js
+++ fe/src/assets/styles/themes/default.js
@@ -1,3 +1,11 @@
 export default {
-  backgroundColor: '#F5F5FC',
+  colors: {
+    background: '#F5F5FC',
+    primary: {
+      lighter: '#e0e3ff',
+      light: '#6674f4',
+      main: '#5061fc',
+      dark: '#3346f0',
+    },
+  },
 };
