a1ab6a340ab212e1c5a0186979c52706b9faa9ce
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-04-06 23:45:11 -0300

    Criada a listagem dos contatos
    Foi criada a listagem dos contatos assim como o sort, create, edit e delete (apenas a parte visual)

--- fe/src/components/ContactsList/index.js
+++ fe/src/components/ContactsList/index.js
@@ -1,4 +1,8 @@
-import { Container, Header } from './style';
+import { Container, Header, ListContainer, Card } from './style';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export function ContactsList() {
   return (
@@ -7,6 +11,52 @@ export function ContactsList() {
         <strong>3 Contatos</strong>
         <a href="/">Novo contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>João Pedro Luz</strong>
+              <small>Instagram</small>
+            </div>
+            <span>joao.pedro.luz@hotmail.com</span>
+            <span>4002-8922</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+              <button type="button">
+                <img src={trash} alt="Delete" />
+              </button>
+            </a>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>João Pedro Luz</strong>
+              <small>Instagram</small>
+            </div>
+            <span>joao.pedro.luz@hotmail.com</span>
+            <span>4002-8922</span>
+          </div>
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+              <button type="button">
+                <img src={trash} alt="Delete" />
+              </button>
+            </a>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
