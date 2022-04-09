34b77778802e901e68beae6eb85a912d8cba3450
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-04-09 18:37:44 -0300

    Criado middleware do CORS
    Liberando o CORS para o acesso do frontend e mudando a porta padrão do backend

--- api/src/index.js
+++ api/src/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors')
 const routes = require('./routes');
+const errorHandler = require('./app/middlewares/errorHandler')
 
 const app = express();
 
 app.use(express.json());
+app.use(cors)
 app.use(routes);
-app.use((error, req, res, next) => {
-  console.log('#####ERROR HANDLER#####');
-  console.log(error);
-  res.sendStatus(500);
-});
+app.use(errorHandler);
 
-app.listen(3000, () => console.log(`Server started at http://localhost:3000`));
+app.listen(3001, () => console.log(`Server started at http://localhost:3001`));
